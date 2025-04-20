d00dcdbdc6b0af4c72a854adb6f22f14bb891d17
Humbetli Mehemmed Cabir <Humbe_li59@itstep.edu.az>
2025-04-20 15:00:32 +0400

    aded some changes file system

---
+++ Repository/Repositories/IFileUploadRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+
+namespace Repository.Repositories;
+
+public interface IFileUploadRepository
+{
+    Task AddAsync(Domain.Entities.File file);
+    Task Remove(int id);
+    Task<Domain.Entities.File> GetByIdAsync(int id);
+    Task<Domain.Entities.File> GetByNameAsync(string name);
+}
